a13764638c4e887e1a8bb20b536873ab21b89f0a
pmmer2 <80494233+pmmer2@users.noreply.github.com>
2024-12-03 14:08:36 -0500

    Update Ultrasonic.ino

--- Test codes/Ultrasonic/Ultrasonic.ino
+++ Test codes/Ultrasonic/Ultrasonic.ino
@@ -1,1 +1,29 @@
+const int trigPin = 7;
+const int echoPin = 6;
+long duration;
+int distance;
+
+void setup() {
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    Serial.begin(9600);
+    Serial.println("Ultrasonic Sensor Test");
+}
+
+void loop() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    duration = pulseIn(echoPin, HIGH);
+    distance = duration * 0.034 / 2;
+
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+
+    delay(500);
+}
 
